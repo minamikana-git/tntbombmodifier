08805cef405c73b9e76ffb2ec53817206149cbd6
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-05-02 14:50:07 +0900

    1.19.4 support

--- src/main/java/org/hotal/tntbombmodifier/tntbombmodifier.java
+++ src/main/java/org/hotal/tntbombmodifier/tntbombmodifier.java
@@ -1,17 +1,33 @@
 package org.hotal.tntbombmodifier;
 
+import org.bukkit.Location;
+import org.bukkit.entity.TNTPrimed;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Tntbombmodifier extends JavaPlugin {
+public class tntbombmodifier extends JavaPlugin implements Listener {
+
+    // TNTの爆発範囲（半径）を変更する
+    private double explosionRadius = 5.0;
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+
+    @EventHandler
+    public void onTNTPrime(ExplosionPrimeEvent event) {
+        if (event.getEntity() instanceof TNTPrimed) {
+            TNTPrimed tnt = (TNTPrimed) event.getEntity();
+            Location location = tnt.getLocation();
+            // 爆発範囲（半径）を変更する
+            event.setRadius((float) explosionRadius);
+        }
     }
 }
