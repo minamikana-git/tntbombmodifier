fec57e7acf228c2012bc5db65023688f95c69134
蛍 <93361361+minamikana-git@users.noreply.github.com>
2023-05-02 14:49:01 +0900

    master

---
+++ src/main/java/org/hotal/tntbombmodifier/tntbombmodifier.java
@@ -0,0 +1,17 @@
+package org.hotal.tntbombmodifier;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Tntbombmodifier extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
